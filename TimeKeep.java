d4ea7b778835fd852f71bbb08a686ab07d6b7c3d
Morris <morrisa6@lancs.local>
2017-02-14 17:04:36 +0000

    Created Timer that counts in seconds in the cmd

---
+++ TimeKeep.java
@@ -0,0 +1,16 @@
+public class TimeKeep{
+	int count = 0;
+}
+/*public static void main(String[] args)
+{
+	while(0<1){ 
+		try { 
+		Thread.sleep(1000); 
+		count ++; 
+		System.out.println(count);
+		} 
+		catch (InterruptedException e) { 
+		e.printStackTrace(); 
+		}	 
+	}
+}*/
